9a609b379f3a7da0da08cc84559f868614fba9c6
NiloJr13 <nilo.castillano@sun-asterisk.com>
2022-09-21 16:08:50 +0800

    [BULL-2-001] Added store function in AuthController

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'username' => ['required', 'unique:users', 'max:255'],
+            'password' => ['required', 'confirmed'],
+            'email' => ['required', 'unique:users'],
+            'avatar' => ['max:255'],
+        ]);
+
+        $user = User::create([
+            'username' => $validatedData['username'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'avatar' => $validatedData['avatar'],
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
