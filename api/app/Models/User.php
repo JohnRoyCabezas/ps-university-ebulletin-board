e0a97c14678990dc06bb9bb3ef9b6a2fa6cc04d6
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2022-09-27 17:03:09 +0800

    [009][BE]-002 Added relationships in User model

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -44,4 +44,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function department() {
+        return $this->belongsTo(Department::class);
+    }
+    
+    public function chats() {
+        return $this->hasMany(Chat::class);
+    }
+
+    public function files() {
+        return $this->hasMany(File::class);
+    }
+
+    public function roleUsers() {
+        return $this->hasMany(RoleUser::class);
+    }
+
+    public function courseUsers() {
+        return $this->hasMany(CourseUser::class);
+    }
+
+    public function threads() {
+        return $this->hasMany(Thread::class);
+    }
 }
