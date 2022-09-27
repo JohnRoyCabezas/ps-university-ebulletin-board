deb7e0e7ebed3c96b063f9743196579e9227bf1d
John <johncabz4199@gmail>
2022-09-27 13:48:17 +0800

    [008][Markup]-002-Input field will glow on click

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -7,9 +7,7 @@ const LoginPage = () => {
       <NavBar />
       <div className="relative flex flex-col justify-center overflow-hidden mt-20">
         <div className="w-full p-6 m-auto bg-custom-gray rounded-md shadow-md lg:max-w-xl h-96">
-          <h1 className="text-3xl font-bold text-center">
-            Login
-          </h1>
+          <h1 className="text-3xl font-bold text-center">LOGIN</h1>
           <form className="mt-6">
             <div className="mb-4">
               <label
@@ -19,8 +17,9 @@ const LoginPage = () => {
                 Email
               </label>
               <input
+                placeholder="johndoe@gmail.com"
                 type="email"
-                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
             <div className="mb-2">
@@ -31,8 +30,9 @@ const LoginPage = () => {
                 Password
               </label>
               <input
+                placeholder="password"
                 type="password"
-                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
             <div className="mt-16">
