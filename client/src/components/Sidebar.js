e7385260e34d10a179ac166feabfa9f090ad7612
John <johncabz4199@gmail>
2022-09-29 14:16:25 +0800

    [014][Markup]-004-Added sticky top on sidebar and removed comments to be added later

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,108 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { useState } from "react";
-
-// TODO
-// const Modal = ({ isShowing, hide }) =>
-//   isShowing
-//     ? ReactDOM.createPortal(
-//         <React.Fragment>
-//           <div className="modal-overlay" />
-//           <div
-//             className="modal-wrapper bg-dark-blue flex flex-col shadow w-60 text-white z-index:9999999"
-//             aria-modal
-//             aria-hidden
-//             tabIndex={-1}
-//             role="dialog"
-//           >
-//             <div className="modal">
-//               <div className="modal-header">
-//                 <button
-//                   type="button"
-//                   className="modal-close-button"
-//                   data-dismiss="modal"
-//                   aria-label="Close"
-//                   onClick={hide}
-//                 >
-//                   <span aria-hidden="true">&times;</span>
-//                 </button>
-//               </div>
-//               <ul className="pt-2 pb-4 space-y-1 text-sm">
-//                 <li className="rounded-sm">
-//                   <a
-//                     href="#"
-//                     className="flex items-center p-2 space-x-3 rounded-md"
-//                   >
-//                     <svg
-//                       xmlns="http://www.w3.org/2000/svg"
-//                       className="w-6 h-6"
-//                       fill="none"
-//                       viewBox="0 0 24 24"
-//                       stroke="currentColor"
-//                       strokeWidth={2}
-//                     >
-//                       <path
-//                         strokeLinecap="round"
-//                         strokeLinejoin="round"
-//                         d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-//                       />
-//                       <path
-//                         strokeLinecap="round"
-//                         strokeLinejoin="round"
-//                         d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-//                       />
-//                     </svg>
-//                     <span>Settings</span>
-//                   </a>
-//                 </li>
-//                 <li className="rounded-sm">
-//                   <a
-//                     href="#"
-//                     className="flex items-center p-2 space-x-3 rounded-md"
-//                   >
-//                     <svg
-//                       xmlns="http://www.w3.org/2000/svg"
-//                       className="w-6 h-6"
-//                       fill="none"
-//                       viewBox="0 0 24 24"
-//                       stroke="currentColor"
-//                       strokeWidth={2}
-//                     >
-//                       <path
-//                         strokeLinecap="round"
-//                         strokeLinejoin="round"
-//                         d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-//                       />
-//                     </svg>
-//                     <span>Logout</span>
-//                   </a>
-//                 </li>
-//               </ul>
-//             </div>
-//           </div>
-//         </React.Fragment>,
-//         document.body
-//       )
-//     : null;
-
-// const useModal = () => {
-//   const [isShowing, setIsShowing] = useState(false);
-
-//   function toggle() {
-//     setIsShowing(!isShowing);
-//   }
-
-//   return {
-//     isShowing,
-//     toggle,
-//   };
-// };
 
 export default function Sidebar() {
-  // const { isShowing, toggle } = useModal();
   return (
     <div className="flex">
-      <div className="flex flex-col h-screen bg-regal-blue shadow w-60 text-white">
+      <div className="flex flex-col h-screen bg-regal-blue shadow w-60 text-white sticky top-0">
         <div className="space-y-3">
           <div className="flex items-center m-5">
             <h2 className="text-xl font-bold">Sun* University</h2>
@@ -197,7 +98,6 @@ export default function Sidebar() {
               alt="Avatar"
             />
             <label className="mx-2">Avatar</label>
-            {/* <button className="button-default" onClick={toggle}> */}
             <button className="button-default">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -210,7 +110,6 @@ export default function Sidebar() {
                 <path d="M18 15l-6-6-6 6" />
               </svg>
             </button>
-            {/* <Modal isShowing={isShowing} hide={toggle} /> */}
           </div>
         </div>
       </div>
