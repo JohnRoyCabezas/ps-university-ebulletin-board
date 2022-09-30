c2da8133452295b356c505bb6bc609468ca876ea
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2022-09-30 18:29:08 +0800

    [019][Bugfix]-001 Added dynamic input checker for register page

--- client/src/routes/RoutesList.js
+++ client/src/routes/RoutesList.js
@@ -2,11 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import React from 'react';
 
 import AnnouncementPage from "../pages/AnnouncementPage";
+import RegisterPage from "../pages/RegisterPage";
+import LoginPage from "../pages/LoginPage";
 
 const RoutesList = () => {
   return (
     <Routes>
       <Route path="/" element={<AnnouncementPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   )
 }
