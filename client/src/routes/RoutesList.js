df62e31841307c0a47b310e775b966111ad277d9
John <johncabz4199@gmail>
2022-10-05 18:13:58 +0800

    [001][Markup]-001-Create landing page markup

--- client/src/routes/RoutesList.js
+++ client/src/routes/RoutesList.js
@@ -5,6 +5,7 @@ import AdminAnnouncementPage from "../pages/AdminAnnouncementPage";
 import AnnouncementPage from "../pages/AnnouncementPage";
 import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
+import LandingPage from "../pages/LandingPage";
 
 const RoutesList = () => {
   return (
@@ -13,7 +14,7 @@ const RoutesList = () => {
       <Route path="/announcement" element={<AnnouncementPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<LoginPage />}></Route>
+      <Route path="/" element={<LandingPage />}></Route>
     </Routes>
   )
 }
