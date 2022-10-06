7fa1e1b02717b0d57dcc559bf180244a4d7f3e97
John <johncabz4199@gmail>
2022-10-06 21:37:51 +0800

    [028][Markup]-001-Create markup for Create College Page

--- client/src/routes/RoutesList.js
+++ client/src/routes/RoutesList.js
@@ -6,12 +6,14 @@ import AnnouncementPage from "../pages/AnnouncementPage";
 import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
 import LandingPage from "../pages/LandingPage";
+import CreateCollegePage from "../pages/CreateCollegePage";
 import AdminSettingsPage from "../pages/AdminSettingsPage";
 
 const RoutesList = () => {
   return (
     <Routes>
       <Route path="/adminannouncement" element={<AdminAnnouncementPage />} />
+      <Route path="/createcollege" element={<CreateCollegePage />} />
       <Route path="/announcement" element={<AnnouncementPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
