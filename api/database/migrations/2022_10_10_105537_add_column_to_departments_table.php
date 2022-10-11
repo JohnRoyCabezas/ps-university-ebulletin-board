062ef80d4c496a4aa2afc425e05976996cd6db5a
NiloJr13 <nilo.castillano@sun-asterisk.com>
2022-10-11 11:03:36 +0800

    [026][BE]-001 Creation of DepartmentController

---
+++ api/database/migrations/2022_10_10_105537_add_column_to_departments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('departments', function (Blueprint $table) {
+            $table->after('department', function ($table) {
+                $table->foreignId('user_id')->nullable()->constrained();
+                $table->longText('department_information')->nullable();
+            });
+        });
+    }/**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('departments', function (Blueprint $table) {
+            //
+        });
+    }
+};
