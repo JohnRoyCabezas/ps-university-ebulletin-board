6cce36313118872f2b38c83e267a8ae909ab0755
NiloJr13 <nilo.castillano@sun-asterisk.com>
2022-10-24 11:18:06 +0800

    [012] Edit icon disappears even after RichtextEditor is closed on Announcement Page

--- client/src/components/AdminMessageOptions.js
+++ client/src/components/AdminMessageOptions.js
@@ -9,16 +9,14 @@ import { useState } from "react";
 import DeleteModal from "./DeleteModal";
 
 export default function AdminMessageOptions(props) {
-  const [state, setState] = useState(false);
   const [showModal, setShowModal] = useState(false);
-
-  function handleDelete() {
-    setShowModal(true);
-  }
 
   function handleEdit(id) {
     props.handleEdit(id);
-    setState(true);
+  }
+
+  function handleDelete() {
+    setShowModal(true);
   }
 
   function yesDelete() {
@@ -32,30 +30,38 @@ export default function AdminMessageOptions(props) {
   return (
     <>
       <div className="absolute top-0 right-0 translate-y-1/2 -translate-x-1/2 drop-shadow-md px-2 py-0.5 bg-white text-gray-500 border-regal-blue border-2 rounded cursor-pointer">
+
+        {/* Show thread button */}
         <span className="m-1">
           <button onClick={buttonHandler} className="cursor-pointer">
             <FontAwesomeIcon icon={faComment} size="lg" color="#162750" />
           </button>
         </span>
+
+        {/* Edit button */}
         <span className="m-1">
           <button
-            className={state ? "hidden" : "cursor-pointer"}
+            className="cursor-pointer"
             onClick={() => handleEdit(props.id)}
           >
             <FontAwesomeIcon icon={faPenToSquare} size="lg" color="#162750" />
           </button>
         </span>
+
+        {/* Delete button */}
         <span className="m-1">
           <button className="cursor-pointer" onClick={() => handleDelete()}>
             <FontAwesomeIcon icon={faTrashAlt} size="lg" color="#162750" />
           </button>
         </span>
+
       </div>
       <div>
         {showModal && (
           <DeleteModal
             message="Are you sure you want to remove this message?"
             buttonConfirmText="Yes"
+            cancel={props.cancel}
             buttonCancelText="No"
             setShowModal={setShowModal}
             delete={() => yesDelete()}
--- client/src/components/RichTextEditor.js
+++ client/src/components/RichTextEditor.js
@@ -1,67 +1,109 @@
-import { React, useState, useEffect } from "react";
+import { React, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowAltCircleRight } from "@fortawesome/free-regular-svg-icons";
-import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
+import { faPaperclip, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import AnnouncementApi from "../api/AnnouncementApi";
 
 export default function NavBar(props) {
 
   const [announcement, setAnnouncement] = useState('');
-  const [errMsg, setErrMsg] = useState('');
   const [isEdit, setIsEdit] = useState(props.isEdit);
+  const [buttonState, setButtonState] = useState(false);
+  const [editState, setEditState] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!props.isEdit && !isEdit) {
-      AnnouncementApi.createAnnouncement(
-        {
-          announcementable_id: props.params.announcementable_id,
-          announcementable_type: props.params.announcementable_type,
-          announcement: announcement
-        }).then(
-          (res) => {
-            setAnnouncement('');
-            props.handleRefresh(res.data.announcement);
-          },
-          (err) => {
-            setErrMsg(err.response.data.errors);
-          }
-        )
-    }
-
-    else {
-      AnnouncementApi.updateSpecificAnnouncement(props.id, { announcement_update: announcement }).then(
+    setButtonState(true);
+    AnnouncementApi.createAnnouncement(
+      {
+        announcementable_id: props.params.announcementable_id,
+        announcementable_type: props.params.announcementable_type,
+        announcement: announcement
+      }).then(
         (res) => {
-          props.isChange(false)
+          setAnnouncement('');
+          props.handleRefresh(res.data.announcement);
+          setButtonState(false)
         }
       )
-    }
+  }
+
+  function handleEdit() {
+    setButtonState(true);
+    AnnouncementApi.updateSpecificAnnouncement(props.id, { announcement_update: announcement }).then(
+      () => {
+        props.isChange(false)
+      }
+    )
   }
 
   const handleChange = (e) => {
     setAnnouncement(e);
     setIsEdit(false);
+    setEditState(true);
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <ReactQuill theme="snow" placeholder={"Write a comment..."} value={(isEdit) ? props.params.announcement : announcement} onChange={handleChange} className="block bottom-0"></ReactQuill>
+
+        <ReactQuill
+          theme="snow"
+          placeholder={"Write a comment..."}
+          value={(isEdit) ? props.params.announcement : announcement}
+          onChange={handleChange}
+          className="block bottom-0">
+        </ReactQuill>
+
         <div className="flex justify-between rte mb-2 p-2">
           <FontAwesomeIcon
             icon={faPaperclip}
             size="2x"
             color="#162750"
           />
-          <button disabled={!(announcement) ? true : false} type="submit">
-            <FontAwesomeIcon
-              icon={faArrowAltCircleRight}
-              size="2x"
-              color="#162750"
-            />
-          </button>
+          {
+            props.isEdit ?
+              <>
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => props.cancel()}
+                    className="text-gray-900 bg-white hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleEdit()}
+                    className={`text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ${editState ? 'bg-blue-700 hover:bg-blue-800' : 'bg-blue-400'}`}
+                    disabled={!editState}
+                  >
+                    {
+                      !buttonState ?
+                        "Update"
+                        :
+                        <FontAwesomeIcon icon={faSpinner} size="1x" color="white" spin />
+                    }
+
+                  </button>
+                </div>
+              </>
+              :
+              <button
+                className={`text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ${!(announcement) || (announcement === "<p><br></p>") || buttonState ? 'bg-blue-400' : 'bg-blue-700 hover:bg-blue-800'}`}
+                disabled={!(announcement) || announcement === "<p><br></p>" || buttonState ? true : false}
+                type="submit"
+              >
+                {
+                  !buttonState ?
+                    "Send"
+                    :
+                    <FontAwesomeIcon icon={faSpinner} size="1x" color="white" spin />
+                }
+              </button>
+
+          }
         </div>
       </form>
     </div>
