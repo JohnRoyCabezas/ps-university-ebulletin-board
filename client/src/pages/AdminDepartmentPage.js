8a819a081f23d13c5b2ab156c5bd166224ba48ef
arden-dave <ardendave.cabotaje@sun-asterisk.com>
2022-10-28 10:25:37 +0800

    [072][FE]-001 Markup and integration of announcement fetching on Admin Department page

--- client/src/pages/AdminDepartmentPage.js
+++ client/src/pages/AdminDepartmentPage.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react';
+import AnnouncementCard from '../components/AnnouncementCard';
+import RichTextEditor from '../components/RichTextEditor';
+import Thread from "../components/Thread";
+import AnnouncementApi from '../api/AnnouncementApi';
+import DepartmentApi from '../api/DepartmentApi';
+import { useParams } from 'react-router-dom';
 
-const AdminDepartmentPage = () => {
+const AdminDepartment = () => {
+  const { departmentid } = useParams();
+  const [isThread, setThread] = useState(false);
+  const [announcementThread, setAnnouncementThread] = useState()
+  const [announcements, setAnnouncement] = useState([]);
+  const [department, setDepartment] = useState()
+  const params =
+  {
+    announcementable_id: departmentid,
+    announcementable_type: "App/Models/Department",
+  }
+
+  function setThreadValue(value) {
+    setThread(value);
+  }
+
+  useEffect(() => {
+    AnnouncementApi.fetchChannelAnnouncements(params).then(
+      (res) => {
+        setAnnouncement(res.data);
+      }
+    );
+    DepartmentApi.fetchSpecificDepartment(departmentid).then(
+      ({data})  => {
+        console.log(data)
+        setDepartment(data)
+      }
+    );
+  }, [departmentid]);
+
+  useEffect(() => {
+    const lastDiv = document.getElementById("announcementWrapper");
+    lastDiv.scrollTo(0, lastDiv.scrollHeight)
+  }, [announcements])
+
+  function handleRefresh() {
+    AnnouncementApi.fetchChannelAnnouncements(params).then(
+      (res) => {
+        setAnnouncement(res.data);
+      }
+    );
+  }
   return (
-    <div>ADMINDepartmentPage</div>
-  )
-}
+    <div className="flex h-screen">
+      <div className="relative flex flex-col w-full">
+        <h1 className="absolute top-0 z-50 w-full font-bold p-3 text-lg bg-white border-b-2">{department?.department} Announcements</h1>
+        <div className="flex flex-col justify-between h-full">
+          <div id='announcementWrapper' className="mt-12 overflow-y-auto">
+            {
+              announcements.map((announcement) => (
+                <AnnouncementCard
+                    key={announcement.id.toString()}
+                    userRole={'admin'}
+                    announcement={announcement}
+                    handleRefresh={() => handleRefresh()}
+                    setValue={setThreadValue}
+                    setAnnouncementThread={setAnnouncementThread}
+                />
+              ))}
+          </div>
+          <div className="p-2 rounded-3xl">
+            <RichTextEditor handleRefresh={() => handleRefresh()} params={params} />
+          </div>
+        </div>
+      </div>
+      {isThread && <Thread userRole={'student'} setValue={setThreadValue} announcementThread={announcementThread}/>}
+    </div>
+  );
+};
 
-export default AdminDepartmentPage;
+export default AdminDepartment;
