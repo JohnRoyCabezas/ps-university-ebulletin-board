332812122c24e510ccc2678f07d3c24b5663cfd8
arden-dave <ardendave.cabotaje@sun-asterisk.com>
2022-11-04 13:41:25 +0800

    [018][Bugfix][FE]-001 University announcement no longer fetch other university announcements

--- client/src/pages/AdminAnnouncementPage.js
+++ client/src/pages/AdminAnnouncementPage.js
@@ -3,14 +3,15 @@ import Thread from "../components/Thread";
 import AnnouncementCard from "../components/AnnouncementCard";
 import RichTextEditor from "../components/RichTextEditor";
 import AnnouncementApi from "../api/AnnouncementApi";
-
+import Cookies from "js-cookie";
 
 const AdminAnnouncementPage = () => {
   const [isThread, setThread] = useState(false);
   const [announcementThread, setAnnouncementThread] = useState()
   const [announcements, setAnnouncement] = useState([]);
+  const university_id = Cookies.get('universityid');
   const params = {
-    announcementable_id: 1,
+    announcementable_id: university_id,
     announcementable_type: "App/Models/University",
   };
 
@@ -43,9 +44,7 @@ const AdminAnnouncementPage = () => {
     <div className="flex h-screen">
       <div className="relative flex flex-col w-full">
         <div className="absolute top-0 z-50 w-full font-bold flex justify-between p-3 text-2xl bg-white border-b-2">
-          {/* <span className="text-lg">{time.getHours() > 5 && time.getHours() < 20 ? "🌞" : "🌙"} {time.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</span> */}
           <h1> Announcements</h1>
-          {/* <span className="botton-0 mr-6 text-sm font-normal">📆 <span className="italic">{time.toLocaleString([], {month: 'long', day: '2-digit'})}, {time.getFullYear()}</span></span> */}
         </div>
         <div className="flex flex-col justify-between h-full">
           <div id='announcementWrapper' className="mt-12 overflow-y-auto bg-danger">
