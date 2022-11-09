cb0388806312b6b85831b742aba9230fadf83b61
NiloJr13 <nilo.castillano@sun-asterisk.com>
2022-11-09 10:24:26 +0800

    [003][Hotfix]-001 Added default image for ThreadsHeader

--- client/src/components/ThreadsHeader.js
+++ client/src/components/ThreadsHeader.js
@@ -6,6 +6,7 @@ const ThreadsHeader = ({thread}) => {
   return (
     <div className="relative flex bg-white w-full py-4 px-6">
       <img
+        onError={(e) => e.target.src = 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png?w=360'}
         src={thread?.user?.avatar}
         className="rounded-full w-12 h-12"
         alt="Avatar"
