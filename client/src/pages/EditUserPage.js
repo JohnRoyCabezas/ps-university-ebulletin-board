9cbbf879f5a7ed4e9eeab9bd5e4f426936c48252
arden-dave <ardendave.cabotaje@sun-asterisk.com>
2022-11-28 17:15:02 +0800

    [519][Bugfix]-001 Applied default avatar image

--- client/src/pages/EditUserPage.js
+++ client/src/pages/EditUserPage.js
@@ -1,23 +1,25 @@
-import Cookies from 'js-cookie';
-import { useEffect, useState } from 'react';
-import AuthApi from '../api/AuthApi';
-import Dropdown from '../components/Dropdown';
-import RoleApi from '../api/RoleApi';
-import DepartmentApi from '../api/DepartmentApi';
-import { useNavigate, useParams } from 'react-router-dom';
-import SuccessModal from '../components/SuccessModal';
-import { AvatarUploadApi } from '../api/AvatarUploadApi';
-import BackButton from '../components/BackButton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import SubmitButton from '../components/submitButton';
+import Cookies from "js-cookie";
+import { useContext, useEffect, useState } from "react";
+import AuthApi from "../api/AuthApi";
+import Dropdown from "../components/Dropdown";
+import RoleApi from "../api/RoleApi";
+import DepartmentApi from "../api/DepartmentApi";
+import { useNavigate, useParams } from "react-router-dom";
+import SuccessModal from "../components/SuccessModal";
+import { AvatarUploadApi } from "../api/AvatarUploadApi";
+import BackButton from "../components/BackButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import SubmitButton from "../components/submitButton";
+import { UserContext } from "../utils/UserContext";
 const EditUserPage = () => {
+  const { refetchUser } = useContext(UserContext);
   const navigate = useNavigate();
   const initialParams = {
-    fullname: '',
-    email: '',
-    department_id: '',
-    role_id: '',
+    fullname: "",
+    email: "",
+    department_id: "",
+    role_id: "",
   };
   const { id } = useParams();
   const [departments, setDepartments] = useState([]);
@@ -25,7 +27,7 @@ const EditUserPage = () => {
   const [showModal, setShowModal] = useState(false);
   const [params, setParams] = useState(initialParams);
   const [valid, setValid] = useState({ format: false, size: false });
-  const university_id = Cookies.get('universityid');
+  const university_id = Cookies.get("universityid");
   const [processing, setProcessing] = useState(false);
   useEffect(() => {
     DepartmentApi.fetchDepartments(university_id).then((res) => {
@@ -40,7 +42,7 @@ const EditUserPage = () => {
         avatar: res.data?.avatar,
         fullname: res.data?.fullname,
         email: res.data?.email,
-        department_id: res.data?.department_id ? res.data?.department_id : '',
+        department_id: res.data?.department_id ? res.data?.department_id : "",
         role_id: res.data?.role_user?.role?.id,
       });
     });
@@ -48,49 +50,59 @@ const EditUserPage = () => {
   const handleInputChange = (e) => {
     setParams({ ...params, [e.target.name]: e.target.value });
     Cookies.set(
-      'params',
+      "params",
       JSON.stringify({ ...params, [e.target.name]: e.target.value })
     );
   };
   const handleSelectChange = (type, item) => {
-    if (type === 'department') {
+    if (type === "department") {
       setParams({ ...params, department_id: item.value });
       Cookies.set(
-        'params',
+        "params",
         JSON.stringify({ ...params, department_id: item.value })
       );
-    } else if (type === 'role') {
+    } else if (type === "role") {
       setParams({ ...params, role_id: item.value });
-      Cookies.set('params', JSON.stringify({ ...params, role_id: item.value }));
+      Cookies.set("params", JSON.stringify({ ...params, role_id: item.value }));
     }
   };
 
   const removeAvatar = () => {
-    setParams({ ...params, avatar: '0' });
-    setValid({size:false, format:false})
-  }
+    setParams({ ...params, avatar: "0" });
+    setValid({ size: false, format: false });
+  };
 
   const uploadAvatar = (uploadedAvatar) => {
-    if (['jpg', 'png', 'jpeg'].includes(uploadedAvatar.name.substring(uploadedAvatar.name.lastIndexOf(".") + 1))) {
+    if (
+      ["jpg", "png", "jpeg"].includes(
+        uploadedAvatar.name.substring(uploadedAvatar.name.lastIndexOf(".") + 1)
+      )
+    ) {
       const formData = new FormData();
-      formData.append('avatar', uploadedAvatar);
-      AvatarUploadApi
-        .upload({ formData }).then((res) => {
-          setParams({ ...params, avatar: `http://localhost:8000/Avatars/${res.data}` })
+      formData.append("avatar", uploadedAvatar);
+      AvatarUploadApi.upload({ formData })
+        .then((res) => {
+          setParams({
+            ...params,
+            avatar: `http://localhost:8000/Avatars/${res.data}`,
+          });
         })
         .catch(function () {
-          setValid({ ...valid, size: true })
-        })
-    } else { setValid({ ...valid, format: true }) }
-  }
+          setValid({ ...valid, size: true });
+        });
+    } else {
+      setValid({ ...valid, format: true });
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setProcessing(true)
+    setProcessing(true);
     AuthApi.update(id, params).then((res) => {
-      Cookies.remove('params');
+      Cookies.remove("params");
       setShowModal(true);
       setProcessing(false);
+      refetchUser();
     });
   };
 
@@ -100,12 +112,12 @@ const EditUserPage = () => {
         <SuccessModal
           title="Update User"
           message="Successfuly updated user information!"
-          setShowModal={(e) => navigate('/manageusers')}
+          setShowModal={(e) => navigate("/manageusers")}
         />
       )}
       <div className="flex flex-col h-screen w-full">
         <h1 className="font-bold p-3 sticky top-0 bg-white text-lg border-b-2">
-          <BackButton link={'/manageusers'} />
+          <BackButton link={"/manageusers"} />
           Edit User
         </h1>
         <div className="relative h-full flex flex-col justify-center items-center overflow-hidden">
@@ -115,30 +127,55 @@ const EditUserPage = () => {
                 <label className="text-sm font-semibold text-gray-800 flex items-center justify-center mb-2">
                   Avatar
                 </label>
-                <div
-                  className="flex justify-center w-24 h-24"
-                >
+                <div className="flex justify-center w-24 h-24">
                   <img
-                    onError={(e) => e.target.src = 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png?w=360'}
+                    onError={(e) =>
+                      (e.target.src =
+                        "https://cdn-icons-png.flaticon.com/512/1077/1077114.png?w=360")
+                    }
                     src={params?.avatar}
                     className="rounded-full w-24"
-                    alt=''
+                    alt=""
                   />
                 </div>
-                <div className='flex justify-center my-2 flex-col'>
-                  <label htmlFor='dropzone' className='bg-white border text-center rounded px-3 py-1 cursor-pointer hover:bg-gray-200'>Upload Photo</label>
+                <div className="flex justify-center my-2 flex-col">
+                  <label
+                    htmlFor="dropzone"
+                    className="bg-white border text-center rounded px-3 py-1 cursor-pointer hover:bg-gray-200"
+                  >
+                    Upload Photo
+                  </label>
                   <input
-                    id='dropzone'
+                    id="dropzone"
                     onClick={() => setValid(false)}
                     onChange={(e) => {
-                      uploadAvatar(e.target.files[0])
+                      uploadAvatar(e.target.files[0]);
                     }}
-                    type='file'
-                    className='hidden'
+                    type="file"
+                    className="hidden"
                   />
-                  <label onClick={() => { removeAvatar(); setValid(false) }} className='rounded pt-2 text-center text-sm cursor-pointer hover:underline'>Remove Photo</label>
+                  <label
+                    onClick={() => {
+                      removeAvatar();
+                      setValid(false);
+                    }}
+                    className="rounded pt-2 text-center text-sm cursor-pointer hover:underline"
+                  >
+                    Remove Photo
+                  </label>
                 </div>
-                {(valid.format || valid.size) && (valid.format ? <span className='text-red-600 text-xs relative italic p-1'><FontAwesomeIcon icon={faInfoCircle} /> Must be in '.jpeg', '.jpg', and '.png' format</span> : <span className='text-red-600 text-xs relative italic p-1'><FontAwesomeIcon icon={faInfoCircle} /> Avatar size must be 2mb below</span>)}
+                {(valid.format || valid.size) &&
+                  (valid.format ? (
+                    <span className="text-red-600 text-xs relative italic p-1">
+                      <FontAwesomeIcon icon={faInfoCircle} /> Must be in
+                      '.jpeg', '.jpg', and '.png' format
+                    </span>
+                  ) : (
+                    <span className="text-red-600 text-xs relative italic p-1">
+                      <FontAwesomeIcon icon={faInfoCircle} /> Avatar size must
+                      be 2mb below
+                    </span>
+                  ))}
               </div>
               <div className="mb-4">
                 <label className="block text-sm font-semibold text-gray-800 mb-2">
@@ -221,8 +258,11 @@ const EditUserPage = () => {
               <div className="mt-16">
                 <SubmitButton
                   handleSubmit={handleSubmit}
-                  buttonDisabled={!departments || !params.fullname || !params.email
-                    ? false : true}
+                  buttonDisabled={
+                    !departments || !params.fullname || !params.email
+                      ? false
+                      : true
+                  }
                   processing={processing}
                   buttonTitle={"Edit Account"}
                 />
--- client/src/utils/UserContext.js
+++ client/src/utils/UserContext.js
@@ -1,45 +1,59 @@
 import { createContext, useState, useEffect } from "react";
-import UserApi from "../api/UserApi"
+import UserApi from "../api/UserApi";
 
 const UserContext = createContext(null);
 
-const UserContextProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=> {
-        UserApi.fetchUser().then(({data}) => {
-            if(data.theme===null) setUser({...user ,...data, theme: "bg-regal-blue"});
-            else setUser({...user ,...data});
-            setLoading(false);
-        }).catch((error)=>{
-            setLoading(false);
-        })
-    }, [])
+  useEffect(() => {
+    UserApi.fetchUser()
+      .then(({ data }) => {
+        let avatar = data.avatar;
+        if (avatar === null || avatar === "0")
+          avatar =
+            "https://cdn-icons-png.flaticon.com/512/1077/1077114.png?w=360";
+        if (data.theme === null)
+          setUser({ ...user, ...data, avatar, theme: "bg-regal-blue" });
+        else setUser({ ...user, ...data, avatar });
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+      });
+  }, []);
 
-    const setTheme = (theme) => {
-        setUser({...user, theme: theme})
-    }
+  const setTheme = (theme) => {
+    setUser({ ...user, theme: theme });
+  };
 
-    const refetchUser = () => {
-        UserApi.fetchUser().then(({data}) => {
-            if(data.theme===null) setUser({...user ,...data, theme: "bg-regal-blue"});
-            else setUser({...user ,...data});
-            setLoading(false);
-        }).catch((error)=>{
-            setLoading(false);
-        })
-    }
+  const refetchUser = () => {
+    UserApi.fetchUser()
+      .then(({ data }) => {
+        let avatar = data.avatar;
+        if (avatar === null || avatar === "0")
+          avatar =
+            "https://cdn-icons-png.flaticon.com/512/1077/1077114.png?w=360";
+        if (data.theme === null)
+          setUser({ ...user, ...data, avatar, theme: "bg-regal-blue" });
+        else setUser({ ...user, ...data, avatar });
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+      });
+  };
 
-    const logout = () => {
-        setUser(null);
-    }
+  const logout = () => {
+    setUser(null);
+  };
 
-    return (
-        <UserContext.Provider value={{user, setTheme, logout, refetchUser}}>
-            {!loading && children}
-        </UserContext.Provider>
-    )
-}
+  return (
+    <UserContext.Provider value={{ user, setTheme, logout, refetchUser }}>
+      {!loading && children}
+    </UserContext.Provider>
+  );
+};
 
-export {UserContext, UserContextProvider}
+export { UserContext, UserContextProvider };
